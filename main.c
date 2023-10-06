cd9294c384021ad1b5b0b820188315cafc59f680
Salman MARECAR <salman.marecar@esme.fr>
2023-10-06 15:54:27 +0200

    int r

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void move_forward(int distance) {
+    printf("Moving forward %d meters\n", distance);
+}
+
+int main() {
+    move_forward(10);
+    int r= move_forward;
+    return 0;
+}
