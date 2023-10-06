7d5c6baf7e2695bba88dfe63435233e8f3591112
Salman MARECAR <salman.marecar@esme.fr>
2023-10-06 16:12:42 +0200

    comm

--- main.c
+++ main.c
@@ -7,5 +7,6 @@ void move_forward(int distance) {
 int main() {
     move_forward(10);
     int r= move_forward;
+     // r is a pointer to a function
     return 0;
 }
---
+++ robot.h
@@ -0,0 +1,10 @@
+class Robot {
+public:
+    void move_forward() {
+        // implementation of moving forward
+        // move the robot forward by a certain distance
+        // for example, move the robot forward by 10 units
+        int distance = 10;
+        // code to move the robot forward by distance units
+    }
+};
